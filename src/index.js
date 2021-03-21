caee06b597a47cc3d8056df2b18e5884ea24f8ed
deividdy <deividdyn@gmail.com>
2021-03-21 17:22:40 -0300

    feat(consumer): add

--- src/index.js
+++ src/index.js
@@ -10,10 +10,19 @@ import { Kafka } from "kafkajs"
     })
 
     const producer = kafka.producer()
-    //const consumer = kafka.consumer()
+    const consumer = kafka.consumer({ groupId: 'consumer-group-1' })
 
+    await consumer.connect()
     await producer.connect()
     console.log('conected')
+
+    // escuta desde o inicio
+    await consumer.subscribe({ topic: 'meu-topico-legal', fromBeginning: true })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => console.log('meu-topico-legal:', message.value.toString()),
+    })
+    console.log('listened')
 
     await producer.send({
         topic: 'meu-topico-legal',
